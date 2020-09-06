6a58c1ca224a93de4badb41ff744b221d6b1093e
RegularStiv <sar7743@rit.edu>
2020-09-06 14:29:53 -0400

    made a blocky igloo prefab and made two more instances of them and put them into the scene and a partical snow system

---
+++ Snowman PE/Assets/Scripts/IglooScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IglooScript : MonoBehaviour
+{
+    public GameObject igloo;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        igloo = GameObject.Find("bad igloo");
+        Instantiate(igloo, new Vector3(19,3,60), Quaternion.Euler(90, 0, 100));
+        Instantiate(igloo, new Vector3(75,2,10), Quaternion.Euler(90,90,90));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
