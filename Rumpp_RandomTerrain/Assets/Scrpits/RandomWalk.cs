c890d8189cb7ec654472d6a5f104aea896be3908
RegularStiv <sar7743@rit.edu>
2020-09-13 12:22:07 -0400

    done with the perlin exercise

---
+++ Rumpp_RandomTerrain/Assets/Scrpits/RandomWalk.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomWalk : MonoBehaviour
+{
+    public GameObject walker;
+    private bool pressedM;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pressedM = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!pressedM && Input.GetKeyDown("m"))
+        {
+            pressedM = true;
+        }
+        else if (pressedM && Input.GetKeyDown("m"))
+        {
+            pressedM = false;
+        }
+        if (pressedM)
+        {
+            Walk();
+        }
+    }
+
+    void Walk()
+    {
+            walker.transform.Translate(Random.Range(-1f, 1f), 0, Random.Range(-1f, 1f));
+    }
+}
---
+++ Rumpp_RandomTerrain/Assets/Scrpits/TerrainGeneration.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using UnityEngine;
+
+public class TerrainGeneration : MonoBehaviour
+{
+    private TerrainData tData;
+    private Vector3 worldSize;
+    public int resolution = 129;
+    float[,] heightMap;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tData = gameObject.GetComponent<TerrainCollider>().terrainData;
+        worldSize = new Vector3(250, 50, 250);
+        tData.size = worldSize;
+        tData.heightmapResolution = resolution;
+        heightMap = new float[resolution, resolution];
+        tData.SetHeights(0,0, CreateHeights());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    float[,] CreateHeights()
+    {
+        for (int i = 0; i < resolution; i++)
+        {
+            for (int j = 0; j < resolution; j++)
+            {
+                heightMap[i, j] = (float)i/resolution;
+            }
+        }
+        return heightMap;
+    }
+}
