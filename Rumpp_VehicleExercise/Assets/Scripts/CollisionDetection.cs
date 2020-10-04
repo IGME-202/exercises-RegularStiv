cb8aaaf86c6e1b8613b375ecf5c2696545ed8c99
RegularStiv <sar7743@rit.edu>
2020-10-04 16:50:16 -0400

    pe6 done

---
+++ Rumpp_VehicleExercise/Assets/Scripts/CollisionDetection.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum CollisionType
+{
+    AABB,
+    Circle
+}
+
+public class CollisionDetection : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public static bool AABBCollision(SpriteRenderer a, SpriteRenderer b)
+    {
+        if (a.bounds.min.x < b.bounds.max.x && a.bounds.max.x > b.bounds.min.x && a.bounds.max.y > b.bounds.min.y && a.bounds.min.y < b.bounds.max.y)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public static bool CircleCollision(SpriteRenderer a, SpriteRenderer b)
+    {
+        if (Vector2.Distance(a.bounds.center, b.bounds.center) < (a.bounds.size.x + b.bounds.size.x) / 2f)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+}
---
+++ Rumpp_VehicleExercise/Assets/Scripts/CollisionManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionManager : MonoBehaviour
+{
+    public SpriteRenderer[] collisionObjects;
+    CollisionType collision;
+    bool inContact;
+    int indexNumber;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inContact = false;
+        if (collision == CollisionType.AABB)
+        {
+            for (int i = 1; i < collisionObjects.Length; i++)
+            {
+                if (CollisionDetection.AABBCollision(collisionObjects[0], collisionObjects[i]))
+                {
+                    inContact = true;
+                    indexNumber = i;
+                }
+            }
+        }
+        else
+        {
+            for (int i = 1; i < collisionObjects.Length; i++)
+            {
+                if (CollisionDetection.CircleCollision(collisionObjects[0], collisionObjects[i]))
+                {
+                    inContact = true;
+                    indexNumber = i;
+                }
+            }
+        }
+        if (inContact)
+        {
+
+            collisionObjects[indexNumber].color = Color.red;
+            collisionObjects[0].color = Color.red;
+        }
+        else
+        {
+            for (int i = 0; i < collisionObjects.Length; i++)
+            {
+                collisionObjects[i].color = Color.white;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha1) && collision != CollisionType.AABB)
+        {
+            collision = CollisionType.AABB;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2) && collision != CollisionType.Circle)
+        {
+            collision = CollisionType.Circle;
+        }
+    }
+}
